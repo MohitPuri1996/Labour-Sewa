36d13e52070c1f7b16ec852ee869300e9a6ab74a
Gagandeep Singh <igagandeep.s@gmail.com>
2017-01-20 21:27:47 +0530

    Form completed

--- app/src/main/java/com/developers/laboursewa/ChoiceActivity.java
+++ app/src/main/java/com/developers/laboursewa/ChoiceActivity.java
@@ -1,7 +1,10 @@
 package com.developers.laboursewa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChoiceActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class ChoiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
+
+        Button qrScan = (Button) findViewById(R.id.qr);
+        Button manualDetail = (Button) findViewById(R.id.details);
+
+        manualDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ChoiceActivity.this,ManualUserDetail.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/developers/laboursewa/ManualUserDetail.java
@@ -0,0 +1,64 @@
+package com.developers.laboursewa;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class ManualUserDetail extends AppCompatActivity {
+
+    EditText dob;
+    Calendar myCalendar;
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual_user_detail);
+
+        myCalendar = Calendar.getInstance();
+
+
+        dob = (EditText) findViewById(R.id.dob);
+        dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(ManualUserDetail.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.gender, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+    private void updateLabel() {
+        String myFormat = "dd/MM/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        dob.setText(sdf.format(myCalendar.getTime()));
+    }
+}
