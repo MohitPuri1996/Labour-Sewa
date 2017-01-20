0b00b1064b39bb2376280a2404bd3784b2e8c3c4
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2017-01-20 21:35:58 +0530

    "info coming"

--- app/src/main/java/com/developers/laboursewa/ChoiceActivity.java
+++ app/src/main/java/com/developers/laboursewa/ChoiceActivity.java
@@ -1,13 +1,24 @@
 package com.developers.laboursewa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChoiceActivity extends AppCompatActivity {
-
+    Button scan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
+        scan= (Button) findViewById(R.id.qr);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ChoiceActivity.this,ScannerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
