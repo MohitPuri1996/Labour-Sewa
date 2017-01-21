9a68f8c6e574a096f045cc21a7640f1a62a60c2e
Gagandeep Singh <igagandeep.s@gmail.com>
2017-01-21 15:26:13 +0530

    Improved gridview

--- app/src/main/java/com/developers/laboursewa/SkillCategory.java
+++ app/src/main/java/com/developers/laboursewa/SkillCategory.java
@@ -7,9 +7,11 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ListAdapter;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -29,15 +31,8 @@ public class SkillCategory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skill_category);
-        gridView = (GridView) findViewById(R.id.gridview);
 
-        otherJob = (Button) findViewById(R.id.add_job);
-        otherJob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                addJob();
-            }
-        });
+        setTitle("Select Your Job");
 
         skills = new ArrayList<>();
         skills.add("Carpentry");
@@ -48,8 +43,40 @@ public class SkillCategory extends AppCompatActivity {
         skills.add("Brick Layer");
         skills.add("Electrician");
         skills.add("Concrete");
+
+        gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setNumColumns(skills.size());
         customSkillAdapter = new CustomSkillAdapter(this, skills);
         gridView.setAdapter(customSkillAdapter);
+        setDynamicWidth(gridView);
+
+
+        otherJob = (Button) findViewById(R.id.add_job);
+        otherJob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addJob();
+            }
+        });
+
+
+
+    }
+
+    private void setDynamicWidth(GridView gridView) {
+        ListAdapter gridViewAdapter = gridView.getAdapter();
+        if (gridViewAdapter == null) {
+            return;
+        }
+        int totalWidth;
+        int items = gridViewAdapter.getCount();
+        View listItem = gridViewAdapter.getView(0, null, gridView);
+        listItem.measure(0, 0);
+        totalWidth = 1100;
+        totalWidth = totalWidth*items;
+        ViewGroup.LayoutParams params = gridView.getLayoutParams();
+        params.width = totalWidth;
+        gridView.setLayoutParams(params);
     }
 
     public void addJob(){
@@ -59,7 +86,7 @@ public class SkillCategory extends AppCompatActivity {
         dialog.setTitle("Add Your Job Type");
         newJobType = (EditText) findViewById(R.id.jobType);
         dialogOtherJob = (Button) findViewById(R.id.addJob);
-        /*dialogOtherJob.setOnClickListener(new View.OnClickListener() {
+        dialogOtherJob.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 newJobText = newJobType.getText().toString();
@@ -68,7 +95,7 @@ public class SkillCategory extends AppCompatActivity {
                 }
                 else Toast.makeText(SkillCategory.this, "Enter Some Job Type", Toast.LENGTH_SHORT).show();
             }
-        });*/
+        });
 
         //adding button click event
         dismissButton = (Button) dialog.findViewById(R.id.dismiss);
