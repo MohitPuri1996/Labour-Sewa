2f725de4750c3b57710855ac05ff6ec9dc4354f8
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2017-01-21 13:17:35 +0530

    "adapters"

--- app/src/main/java/com/developers/laboursewa/fragments/Tutorials.java
+++ app/src/main/java/com/developers/laboursewa/fragments/Tutorials.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.developers.laboursewa.R;
 
@@ -19,12 +20,15 @@ public class Tutorials extends Fragment {
         // Required empty public constructor
     }
 
-
+    private ListView mList;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tutorials, container, false);
+        View v2= inflater.inflate(R.layout.fragment_tutorials, container, false);
+        mList= (ListView) v2.findViewById(R.id.tutorials);
+
+        return v2;
     }
 
 }
